d27d164f420db131a251ddc560c49c8f590287ef
Justin Jent <jentjr@gmail.com>
2017-02-21 15:42:05 -0500

    initial commit

---
+++ external/manages.py
@@ -0,0 +1,41 @@
+import pyodbc
+import pandas
+
+def read_manages3(mdb_path):
+    """
+	Function to read MANAGES 3.X database and return 
+	groundwater data in Pandas DataFrame for analysis.
+	
+	Parameters
+	----------
+	mdb_path : connection string
+	    The path to the MANAGES 3.x Site.mdb file.
+	
+	Examples
+	--------
+	>>> from enviropy.external import manages
+	>>> cd = manages.read_manages3('H:\INTERNAL\MANAGES_DATA\Cardinal\Cardinal\Site.mdb')
+	"""
+	
+    driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
+    database = mdb_path
+
+    conxn = pyodbc.connect('DRIVER={0};DBQ={1}'.format(driver, database))
+
+    query = """
+    
+    SELECT sample_results.lab_id, sample_results.location_id, 
+	sample_results.sample_date, site_parameters.param_name,
+	sample_results.lt_measure, sample_results.analysis_result, 
+	site_parameters.default_unit 
+	FROM sample_results LEFT JOIN site_parameters 
+	ON sample_results.storet_code = site_parameters.storet_code
+    
+    """
+
+    data = pandas.read_sql(query, conxn)
+	
+    conxn.close()
+
+    return data
+	
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import sys
+from codecs import open
+
+from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register')
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload --universal')
+    sys.exit()
+
+requires = ['pyodbc', 'pandas']
+version = '0.0.1'
+
+def read(f):
+    return open(f, encoding='utf-8').read()
+
+setup(
+    name='enviropy',
+    version=version,
+	description='Statistical methods and plotting tools for environmental data',
+	long_description=read('README.rst'),
+	author='Justin R. Jent',
+	author_email='jentjr@gmail.com',
+	install_requires=requires,
+	packages=['external']
+	)
