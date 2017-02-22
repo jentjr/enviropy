02fb35298c2603c8a8d1e5564f3d8f38398c90ae
Justin Jent <jentjr@gmail.com>
2017-02-22 00:58:46 -0500

    add to documentation for read_manages3

--- enviropy/external/manages.py
+++ enviropy/external/manages.py
@@ -3,39 +3,47 @@ import pandas
 
 def read_manages3(mdb_path):
     """
-	Function to read MANAGES 3.X database and return 
+	Function to read MANAGES 3.X database and return
 	groundwater data in Pandas DataFrame for analysis.
-	
+
 	Parameters
 	----------
 	mdb_path : connection string
 	    The path to the MANAGES 3.x Site.mdb file.
-	
+
+    Returns
+    -------
+    DataFrame :  pandas DataFrame
+        returns a pandas DataFrame.
+
 	Examples
 	--------
 	>>> from enviropy.external import manages
 	>>> cd = manages.read_manages3('H:\INTERNAL\MANAGES_DATA\Cardinal\Cardinal\Site.mdb')
+
+    .. autoclass:: read_manages3
+
 	"""
-	
+
     driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
     database = mdb_path
 
     conxn = pyodbc.connect('DRIVER={0};DBQ={1}'.format(driver, database))
 
     query = """
-    
-    SELECT sample_results.lab_id, sample_results.location_id, 
+
+    SELECT sample_results.lab_id, sample_results.location_id,
 	sample_results.sample_date, site_parameters.param_name,
-	sample_results.lt_measure, sample_results.analysis_result, 
-	site_parameters.default_unit 
-	FROM sample_results LEFT JOIN site_parameters 
+	sample_results.lt_measure, sample_results.analysis_result,
+	site_parameters.default_unit
+	FROM sample_results LEFT JOIN site_parameters
 	ON sample_results.storet_code = site_parameters.storet_code
-    
+
     """
 
     data = pandas.read_sql(query, conxn)
-	
+
     conxn.close()
 
     return data
-	
+
