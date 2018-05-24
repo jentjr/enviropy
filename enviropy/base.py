837bd053b94f71cf9a82ed65f546fa607277cb95
jentjr <jentjr@gmail.com>
2018-05-24 14:11:40 -0400

    remove pandas DataFram subclass

--- enviropy/base.py
+++
@@ -1,8 +0,0 @@
-import pandas
-from enviropy.plots import EnviroPlot
-
-
-class EnviroData(EnviroPlot):
-
-    def __init__(self, df):
-        self.df = df
--- enviropy/plots/plots.py
+++ enviropy/plots/plots.py
@@ -2,19 +2,40 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-class EnviroPlot:
+def boxplot(df, wells, analytes):
+    """
+    boxplot
+    
+    Parameters
+	----------
+	df : pandas DataFrame
+	    pandas DataFrame of environmental data 
+    
+    wells : list of wells to plots
+    
+    analytes : list of wells to plots
+    
+    Returns
+    -------
+    boxplot : boxplot 
+        returns a seaborn boxplot.
 
-    def boxplot(self, wells, analytes):
-        data = self[self.location_id.isin(wells) & self.param_name.isin(analytes)]
-        g = sns.factorplot(
-            x="location_id",
-            y="analysis_result",
-            col="param_name",
-            data=data,
-            kind="box",
-            sharey=False,
-            sharex=False,
-        )
-        g.set_titles("{col_name}")
-        g.set(xlabel="Location ID", ylabel="Analysis Result")
-        plt.show()
+	Examples
+	--------
+	>>> from enviropy.plots import boxplot
+	>>> boxplot(df, wells, analytes)
+    """
+
+    data = df[df.location_id.isin(wells) & df.param_name.isin(analytes)]
+    g = sns.factorplot(
+        x="location_id",
+        y="analysis_result",
+        col="param_name",
+        data=data,
+        kind="box",
+        sharey=False,
+        sharex=False,
+    )
+    g.set_titles("{col_name}")
+    g.set(xlabel="Location ID", ylabel="Analysis Result")
+    plt.show()
