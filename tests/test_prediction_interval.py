c306f29ea4c149714a6da37f6ed8144ce2d503d3
Justin R. Jent <jentjr@gmail.com>
2019-08-01 12:29:26 -0400

    simplify

--- tests/test_prediction_interval.py
+++ tests/test_prediction_interval.py
@@ -2,10 +2,11 @@ import numpy as np
 from numpy.testing import assert_approx_equal
 from enviropy.stats import prediction_interval
 
+x = [10.0, 11.5, 11.0, 10.6, 10.9, 12.0, 11.3, 10.7]
 
 def test_two_sided_prediction_interval():
     lpl, upl = prediction_interval(
-        [10.0, 11.5, 11.0, 10.6, 10.9, 12.0, 11.3, 10.7], alpha=0.05, type="two-sided"
+        x, alpha=0.05, type="two-sided"
     )
     assert_approx_equal(lpl, 9.471463, significant=7)
     assert_approx_equal(upl, 12.528537, significant=7)
@@ -13,7 +14,7 @@ def test_two_sided_prediction_interval():
 
 def test_lower_prediction_interval():
     lpl, upl = prediction_interval(
-        [10.0, 11.5, 11.0, 10.6, 10.9, 12.0, 11.3, 10.7], alpha=0.05, type="lower"
+        x, alpha=0.05, type="lower"
     )
     assert_approx_equal(lpl, 9.775309, significant=7)
     assert_approx_equal(upl, np.Inf)
@@ -21,7 +22,7 @@ def test_lower_prediction_interval():
 
 def test_upper_prediction_interval():
     lpl, upl = prediction_interval(
-        [10.0, 11.5, 11.0, 10.6, 10.9, 12.0, 11.3, 10.7], alpha=0.05, type="upper"
+        x, alpha=0.05, type="upper"
     )
     assert_approx_equal(lpl, np.NINF)
     assert_approx_equal(upl, 12.22469, significant=7)
@@ -29,7 +30,7 @@ def test_upper_prediction_interval():
 
 def test_k_future_obs():
     lpl, upl = prediction_interval(
-        [10.0, 11.5, 11.0, 10.6, 10.9, 12.0, 11.3, 10.7],
+        x,
         alpha=0.05,
         type="upper",
         k_future_obs=10,
